648fd28324d0f161cea0f5ca25a98e5b47475aa7
Vitaly Glibin <glibin.v@gmail.com>
2013-02-01 19:06:09 +0400

    Prepare for debianization

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='hh-tornado-protobuf-utils',
+    version='0.0.1',
+    packages = ['protoctor'],
+)
