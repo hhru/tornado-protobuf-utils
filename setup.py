97f6f9780b8cafcd76f8d9bf5a149dbb4521461b
Andrey Sumin <an.sumin@hh.ru>
2014-10-28 16:21:33 +0300

    HH-47689 add PEP8 test

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python
+# coding=utf-8
 
 from distutils.core import setup
-from setuptools import find_packages
+from setuptools.command.test import test
+
+
+class TestHook(test):
+    def run(self):
+        test.run(self)
+
+        import nose
+        nose.main(argv=['tests', '-v'])
+
 
 setup(
     name='hh-tornado-protobuf-utils',
     version='0.0.1',
-    packages = ['protoctor'],
+    packages=['protoctor'],
+    cmdclass={'test': TestHook},
+    install_requires=[
+        'nose', 'pep8'
+    ]
 )
